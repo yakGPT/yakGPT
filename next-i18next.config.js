85d42fc2aafef6e5fb38fa446846f60a9190f830
zephyr <linjie.zhang@outlook.com>
2023-04-11 20:20:38 +0800

    fix i18n vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,4 +3,9 @@ module.exports = {
     defaultLocale: "en",
     locales: ["en", "zh"],
   },
+  // https://github.com/i18next/next-i18next/issues/1552#issuecomment-981156476
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/public/locales",
 };
