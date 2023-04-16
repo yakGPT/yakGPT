1ed1ed5818ee1420e76326e211c7eb4ad50c85c9
yakGPT <yakkergpt@gmail.com>
2023-04-16 14:12:43 -0400

    Remove 11labs streaming audio

--- stores/ElevenLabs.ts
+++ stores/ElevenLabs.ts
@@ -73,49 +73,6 @@ export const genAudio = async ({
   }
 };
 
-export const genAudioStream = async ({
-  apiKey,
-  text,
-  voiceId,
-}: {
-  apiKey: string;
-  text: string;
-  voiceId: string;
-}): Promise<ReadableStream<Uint8Array>> => {
-  try {
-    var voice = `${BASE_URL}/text-to-speech/${voiceId}/stream`;
-    const response = await fetch(voice, {
-      method: "POST",
-      headers: {
-        Accept: "audio/mpeg",
-        "xi-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
-    if (!response.ok || !response.body) {
-      const readBody = await response.text();
-      let message = readBody;
-      try {
-        const json = JSON.parse(readBody);
-        message = json.detail.message;
-      } catch (e) {}
-      notifications.show({
-        title: "Error",
-        message,
-        color: "red",
-      });
-      throw new Error(
-        `Network response was not ok ${response.ok} ${message} ${response.status}`
-      );
-    }
-    return response.body;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
 export const getVoices = async (apiKey: string): Promise<Voice[]> => {
   try {
     const response = await fetch(`${BASE_URL}/voices`, {
