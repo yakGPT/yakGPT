f1ee57c3184e30410328300a1df3b8add8bc3a7c
yakGPT <yakkergpt@gmail.com>
2023-11-26 13:55:37 -0500

    Revert "gpt-3.5-turbo-16k and model setting (#87)"
    This reverts commit 07de4fc32255e9f47eb6327fbe177ba54184c419.

--- stores/Model.ts
+++ stores/Model.ts
@@ -9,37 +9,17 @@ export const modelInfos: Record<
   "gpt-3.5-turbo": {
     displayName: "ChatGPT-3.5",
     maxTokens: 4096,
-    costPer1kTokens: 0.0015,
+    costPer1kTokens: 0.002,
   },
   "gpt-3.5-turbo-0301": {
     displayName: "ChatGPT-3.5 March 1",
     maxTokens: 4096,
-    costPer1kTokens: 0.0015,
-  },
-  "gpt-3.5-turbo-0613": {
-    displayName: "ChatGPT-3.5 June 13",
-    maxTokens: 4096,
-    costPer1kTokens: 0.0015,
-  },
-  "gpt-3.5-turbo-16k": {
-    displayName: "ChatGPT-3.5 June 13",
-    maxTokens: 16384,
-    costPer1kTokens: 0.003,
-  },
-  "gpt-3.5-turbo-16k-0613": {
-    displayName: "ChatGPT-3.5 June 13",
-    maxTokens: 16384,
-    costPer1kTokens: 0.003,
+    costPer1kTokens: 0.002,
   },
   "gpt-4": {
     displayName: "GPT-4",
     maxTokens: 8192,
-    costPer1kTokens: 0.03,
-  },
-  "gpt-4-0613": {
-    displayName: "GPT-4 June 13",
-    maxTokens: 8192,
-    costPer1kTokens: 0.03,
+    costPer1kTokens: 0.06,
   },
 };
 
