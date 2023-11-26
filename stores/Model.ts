eac84e69a297ea344f9b2533601e2810d739307c
yakGPT <yakkergpt@gmail.com>
2023-11-26 14:48:26 -0500

    Update pricing

--- stores/Model.ts
+++ stores/Model.ts
@@ -7,19 +7,39 @@ export const modelInfos: Record<
   }
 > = {
   "gpt-3.5-turbo": {
-    displayName: "ChatGPT-3.5",
-    maxTokens: 4096,
-    costPer1kTokens: { prompt: 0.002, completion: 0.002 },
+    displayName: "ChatGPT-3.5 Turbo",
+    maxTokens: 16 * 1024,
+    costPer1kTokens: { prompt: 0.001, completion: 0.002 },
   },
-  "gpt-3.5-turbo-0301": {
-    displayName: "ChatGPT-3.5 March 1",
-    maxTokens: 4096,
-    costPer1kTokens: { prompt: 0.002, completion: 0.002 },
+  "gpt-3.5-turbo-16k": {
+    displayName: "ChatGPT-3.5 Turbo (16k)",
+    maxTokens: 16 * 1024,
+    costPer1kTokens: { prompt: 0.001, completion: 0.002 },
+  },
+  "gpt-3.5-turbo-instruct": {
+    displayName: "ChatGPT-3.5 Instruct",
+    maxTokens: 4 * 1024,
+    costPer1kTokens: { prompt: 0.0015, completion: 0.002 },
   },
   "gpt-4": {
     displayName: "GPT-4",
-    maxTokens: 8192,
+    maxTokens: 8 * 1024,
     costPer1kTokens: { prompt: 0.03, completion: 0.06 },
+  },
+  "gpt-4-32k": {
+    displayName: "GPT-4 (32k)",
+    maxTokens: 8 * 1024,
+    costPer1kTokens: { prompt: 0.06, completion: 0.12 },
+  },
+  "gpt-4-1106-preview": {
+    displayName: "GPT-4 Turbo",
+    maxTokens: 128 * 1024,
+    costPer1kTokens: { prompt: 0.01, completion: 0.03 },
+  },
+  "gpt-4-1106-vision-preview": {
+    displayName: "GPT-4 Turbo Vision",
+    maxTokens: 128 * 1024,
+    costPer1kTokens: { prompt: 0.01, completion: 0.03 },
   },
 };
 
@@ -27,5 +47,5 @@ export const getModelInfo = (model: string) =>
   modelInfos[model] || {
     displayName: model,
     maxTokens: 4096,
-    costPer1kTokens: {prompt: 0, completion: 0},
+    costPer1kTokens: { prompt: 0, completion: 0 },
   };
